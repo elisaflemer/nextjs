d85cdf795398b7795dee2e7b57900b7a09bb04af
Elisa Flemer <elisaflemer@gmail.com>
2023-06-04 17:24:29 -0300

    Feed complete

---
+++ app/profile/page.js
@@ -0,0 +1,20 @@
+"use client"
+
+import { useState, useEffect } from "react"
+import { useSession } from "next-auth/react"
+import { useRouter } from "next/navigation"
+
+import Profile from "@components/Profile"
+
+const Profile = () => {
+  return (
+    <Profile
+    name="My"
+    desc="Welcome to your profile!"
+    data={[]}
+    handleEdit={}
+    handleDelete={}/>
+  )
+}
+
+export default Profile
--- components/PromptCard.js
+++ components/PromptCard.js
@@ -3,10 +3,18 @@ import { useState, useEffect } from "react"
 import Image from "next/image"
 import { useSession } from "next-auth/react"
 import { useRouter, usePathname } from "next/navigation"
+import { set } from "mongoose"
 
 const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
 
-    const [copied, setCopied] = useState(false)
+    const [copied, setCopied] = useState('')
+
+    const handleCopy = () => {
+        setCopied(post.prompt)
+        navigator.clipboard.writeText(post.prompt)
+        setTimeout(() => setCopied(''), 3000)
+    }
+
   return (
     <div className="prompt_card">
         <div className="flex justify-between items-start gap-5">
@@ -17,12 +25,12 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
                     <p className="font-inter text-sm text-gray-500">{post.creator.email}</p>
                 </div>
             </div>
-            <div className="copy_btn" onClick={() => {}}>
-                <Image src={copied ? './assets/icons/tick.svg' : './assets/icons/copy.svg'} alt="Copy" width={12} height={12}/>
+            <div className="copy_btn" onClick={handleCopy}>
+                <Image src={copied === post.prompt ? './assets/icons/tick.svg' : './assets/icons/copy.svg'} alt="Copy" width={12} height={12}/>
             </div>
         </div>
-        <p>{post.prompt}</p>
-        <p>{post.tag}</p>
+        <p className='my-4 font-satoshi text-sm text-gray-700'>{post.prompt}</p>
+        <p className="font-inter text-sm blue_gradient cursor-pointer" onClick={() => handleTagClick && handleTagClick(post.tag)}>{post.tag}</p>
     </div>
   )
 }
