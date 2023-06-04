fba9c5ee6f285d066a70ef7d3a59d8729cfd7d2e
Elisa Flemer <elisaflemer@gmail.com>
2023-06-04 18:13:37 -0300

    Rendering prompts on profile page

--- app/api/users/[id]/posts/route.js
+++ app/api/users/[id]/posts/route.js
@@ -1,10 +1,13 @@
 import { connectToDB } from "@utils/database";
 import Prompt from "@models/prompt";
 
-export const GET = async (request) => {
+export const GET = async (request, {params}) => {
     try {
         await connectToDB();
-        const prompts = await Prompt.find({}).populate('creator')
+        console.log(params.id)
+        const prompts = await Prompt.find({creator: params.id}).populate('creator')
+        console.log(prompts)
+
         return new Response(JSON.stringify(prompts), { status: 200 })
     } catch (error) {
         console.log("Error getting prompt: ", error.message);
--- app/profile/page.js
+++ app/profile/page.js
@@ -6,15 +6,36 @@ import { useRouter } from "next/navigation"
 
 import Profile from "@components/Profile"
 
-const Profile = () => {
+const MyProfile = () => {
+  const { data: session } = useSession();
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      console.log(session?.user.id)
+      const res = await fetch(`/api/users/${session?.user.id}/posts`);
+      const data = await res.json();
+      setPosts(data);
+    }
+    if (session?.user.id) fetchPosts()
+  }, [])
+
+  const handleEdit = async () => {
+
+  }
+  const handleDelete = async () => {
+    
+  }
+  console.log(posts)
+
   return (
     <Profile
     name="My"
     desc="Welcome to your profile!"
-    data={[]}
-    handleEdit={}
-    handleDelete={}/>
+    data={posts}
+    handleEdit={handleEdit}
+    handleDelete={handleDelete}/>
   )
 }
 
-export default Profile
+export default MyProfile
