044cbfa24cab91f276137e2af697413df9c30434
Elisa Flemer <elisaflemer@gmail.com>
2023-06-04 20:34:09 -0300

    logging

--- components/Feed.js
+++ components/Feed.js
@@ -24,6 +24,7 @@ const Feed = () => {
     const fetchPosts = async () => {
       const res = await fetch("/api/prompt");
       const data = await res.json();
+      console.log(data)
       setPrompts(data);
     }
     fetchPosts()
